720279c57de6b641cfb331bc9d018fdc8f7c3e85
Anthony Hill <hillganthony@outlook.com>
2021-11-27 14:08:15 -0500

    commit tree 6f94d8bb76de1d4bd92b849bd6dac55e4dca2283

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+      t.integer :price
       t.timestamps
     end
   end
